0f96bcb6b35c220b2c168923c123d365a578f29c
dmshel <37098267+dmshel@users.noreply.github.com>
2023-03-09 09:21:00 +0700

    some fix

--- bot.py
+++ bot.py
@@ -2,50 +2,48 @@ from telebot import *
 from buttons import *
 import os
 import yt_dlp
-from yt_dlp import YoutubeDL
 
 TOKEN = ''
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
 
+@bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"Выберите от куда вы хотите скачать видео.", reply_markup = social_network)
+    bot.send_message(message.chat.id, "Выберите от куда вы хотите скачать видео.", reply_markup=social_network)
+
 
 @bot.callback_query_handler(func=lambda call: True)
-
 def choose_social_network(call):
-  if call.data == 'INS':
+    if call.data == 'INS':
         bot.send_message(call.message.chat.id, '❌Пока не Работает🤦‍♂️😢')
 
-  elif call.data == 'YT':
-      bot.send_message(call.message.chat.id, '👋 Пришлите ссылку на YouTube видео и я его скачаю для вас ❤️‍🔥')
+    elif call.data == 'YT':
+        bot.send_message(call.message.chat.id, '👋 Пришлите ссылку на YouTube видео и я его скачаю для вас ❤️‍🔥')
 
 
-      @bot.message_handler()
+@bot.message_handler()
+def YOUTUBE_DOWNLOADER(message):
+    link = message.text
+    bot.send_message(message.chat.id, 'Попробую  это скачать...⌛😉')
+    time.sleep(3)
+    bot.reply_to(message, 'Отправляю.... Ждите🎁')
+    ydlopts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+    }
+    with yt_dlp.YoutubeDL(ydlopts) as ydl:
+        video = ydl.extract_info(
+            link,
+            download=False
+        )
+    videoname = video['title']
+    ydl.download([link])
+    filename = ydl.prepare_filename(video)
 
-      def YOUTUBE_DOWNLOADER(message):
+    bot.send_document(message.chat.id, open(filename, 'rb'),
+                      caption='✔Вот Ваше видео: {}'.format(videoname))
 
-        link = message.text
-        bot.send_message(message.chat.id, 'Попробую  это скачать...⌛😉')
-        time.sleep(3)
-        bot.reply_to(message, 'Отправляю.... Ждите🎁')
-        ydlopts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-        }
-        with yt_dlp.YoutubeDL(ydlopts) as ydl:
-            video = ydl.extract_info(
-                link,
-                download=False
-            )
-        videoname = video['title']
-        ydl.download([link])
-        filename = ydl.prepare_filename(video)
+    os.remove(filename)
 
-        bot.send_document(message.chat.id, open(filename, 'rb'),
-            caption='✔Вот Ваше видео: {}'.format(videoname))
-
-        os.remove(filename)
 
 bot.infinity_polling()
